b8d38606dbf720bb49e702a84efbb324be95c111
Prakarsh Upmanyu <prakarsh@pk-air.home>
2024-09-18 19:16:57 -0400

    inorder traversal
    Signed-off-by: Prakarsh Upmanyu <prakarsh@pk-air.home>

---
+++ python_solutions/94_binary_tree_inorder_traversal.py
@@ -0,0 +1,81 @@
+"""
+https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+
+
+Example 1:
+
+Input: root = [1,null,2,3]
+
+Output: [1,3,2]
+
+Explanation:
+
+
+
+Example 2:
+
+Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+
+Output: [4,2,6,5,7,1,3,9,8]
+
+Explanation:
+
+
+
+Example 3:
+
+Input: root = []
+
+Output: []
+
+Example 4:
+
+Input: root = [1]
+
+Output: [1]
+
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+
+
+Follow up: Recursive solution is trivial, could you do it iteratively?
+"""
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+def inorder_traversal(root: TreeNode):
+    result = []
+
+    def inorder(root):
+        if root is None:
+            return []
+
+        inorder(root.left)
+        result.append(root.val)
+        inorder(root.right)
+    inorder(root)
+    return result
+
+
+if __name__ == "__main__":
+    root = TreeNode(1)
+    root.left = TreeNode(2)
+    root.right = TreeNode(3)
+    root.left.left = TreeNode(4)
+    root.left.right = TreeNode(5)
+    root.right.left = TreeNode(6)
+    root.right.right = TreeNode(7)
+    print(inorder_traversal(root))
